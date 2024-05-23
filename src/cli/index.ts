ba1b6ee1fc640cba462ab52ef919819903e2069f
Charles Yu <charles.yu108@gmail.com>
2024-05-22 22:58:33 -0700

    Fix warnings

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 import { Command } from "commander";
 import ComposeCommand from "./cmd.compose.js";
 import SendCommand from "./cmd.send.js";
