4005e375da85a14f4eef0dc5ef5365c3c28ec769
Charles Yu <charles.yu108@gmail.com>
2024-05-31 16:27:03 -0700

    Move mocks over

--- src/lib/renderers/mock.ts
+++ src/lib/renderers/mock.ts
@@ -1,4 +1,4 @@
-import { getMockEmails } from "../mocks.js";
+import { Email } from "../types.js";
 import { BaseRenderer, RenderInputOptions } from "./base.js";
 
 export class MockRenderer implements BaseRenderer {
@@ -6,4 +6,29 @@ export class MockRenderer implements BaseRenderer {
     return getMockEmails();
   }
 }
+
+const mockResponse = `{
+    "emails": [
+        {
+            "to": "test@gmail.com",
+            "subject": "Hello",
+            "body": "Hello, I found your email test@gmail.com. \\n Thanks."
+        },
+        {
+            "to": "example@exmaple.com",
+            "subject": "Hi",
+            "body": "Hi, I found your email example@example.com. \\n Thanks."
+        }
+    ],
+    "warnings": ["This is a mock response"]
+}
+`;
+
+export const getMockEmails = (): { emails: Email[]; warnings: string[]; } => {
+  const responseJSON = JSON.parse(mockResponse);
+  return {
+    emails: responseJSON.emails ?? [],
+    warnings: responseJSON.warnings ?? [],
+  };
+};
 
